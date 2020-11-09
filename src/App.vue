7d413d78bebe1641e647ba5d650ce843e946b84c
dragonying <qfdy6688@gmail.com>
2020-11-09 11:52:39 +0800

    less 整合

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+//less的整合
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -22,5 +23,11 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+
+    div {
+        color: gold;
+        font-weight: 700;
+    }
+
 }
 </style>
