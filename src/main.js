aa99545e204e8bc5f2b79b185e62a28fdba027db
dragonying <qfdy6688@gmail.com>
2020-11-09 11:08:59 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
 
 Vue.config.productionTip = false
+//引入axios
+import axios from 'axios'
+axios.defaults.baseURL = "";//设置全局api host
+// (3)给Vue的构造函数的原型加了一个属性$axios
+// 所有Vue的实例都有$axios这个属性
+// 规范:如果要给Vue的原型加属性，名字前面加$，方便让人区分是vue实例自己的属性还是原型的属性
+Vue.prototype.$axios = axios;
+
+
+console.log(router)
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+//引入路由
+import VueRouter from 'vue-router';
+
+//解决当前位置的冗余导航报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+}
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: []
+});
+export default router
