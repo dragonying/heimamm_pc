78d2a6c1a3ff578aa65f5e6acc25db1945402d31
dragonying <qfdy6688@gmail.com>
2020-11-13 10:31:05 +0800

    注册

--- src/api/reg.js
+++ src/api/reg.js
@@ -2,12 +2,31 @@ import request from '@/api/request.js';
 
 export function userReg(data = {}, callback = null) {
   return request({
-    url: '/login',
+    url: '/register',
     method: 'post',
     data: data
   }, callback)
 }
-// //验证码方法
+//图形验证码方法
 export function smPinCode() {
   return process.env.VUE_APP_BASEURL + '/captcha?type=sendsms&' + Date.now();
 }
+
+
+//发送短信验证码
+export function sendSms(data = {}, callback = null){
+  return request({
+    url: '/sendsms',
+    method: 'post',
+    data: data
+  }, callback)
+}
+
+//头像上传
+export const upload = {
+  url: process.env.VUE_APP_BASEURL + '/uploads',//上传地址
+  name: 'image', //上传文件字段名
+  acceptType: ['jpg', 'jpeg', 'png', 'gif'],//允许的类型
+  sizeLimit: 4 * 1024 * 1024,//上传大小限制 B
+}
+
--- src/views/login/reg.vue
+++ src/views/login/reg.vue
@@ -1,15 +1,16 @@
 <template>
 <!-- 注册对话框 -->
 <el-dialog title="用户注册" :width='width' center :visible.sync="regDialogShow">
-    <el-form :model="regForm" :rules="rules" :label-width='labelWidth'>
+    <el-form :model="regForm" :rules="rules" ref='regForm' :label-width='labelWidth'>
         <el-form-item label="头像"  prop="avatar">
             <el-upload
             class="avatar-uploader"
-            action="https://jsonplaceholder.typicode.com/posts/"
+            :name="upload.name"
+            :action="upload.url"
             :show-file-list="false"
             :on-success="handleAvatarSuccess"
             :before-upload="beforeAvatarUpload">
-                <img v-if="regForm.avatar" :src="regForm.avatar" class="avatar">
+                <img v-if="tempImgUrl" :src="tempImgUrl" class="head-avatar">
                 <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
             <el-input type="hidden" v-model="regForm.avatar"></el-input>
@@ -23,57 +24,61 @@
         <el-form-item label="手机" prop="phone">
             <el-input v-model="regForm.phone"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="password">
-            <el-input v-model="regForm.password"></el-input>
+        <el-form-item label="密码" prop="password" >
+            <el-input v-model="regForm.password" show-password></el-input>
         </el-form-item>
         <el-form-item label="图形码">
            <el-row>
                 <el-col :span="17">
-                    <el-input ></el-input>
+                    <el-input v-model="regForm.code"></el-input>
                 </el-col>
                 <el-col :span="6" :offset="1">
                     <img class='pin-code' :src="codePic" @click="refleshCode">
                 </el-col>
             </el-row>
         </el-form-item>
-        <el-form-item label="验证码">
+        <el-form-item label="验证码" prop="rcode">
             <el-row>
                 <el-col :span="17">
                     <el-input v-model="regForm.rcode"></el-input>
                 </el-col>
                 <el-col :span="6" :offset="1">
-                      <el-button class='code-btn'>获取用户验证码</el-button>
+                      <el-button class='code-btn' :disabled="delay>0" @click="getPhoneCode">{{codeText}}</el-button>
                 </el-col>
             </el-row>
         </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
         <el-button @click="regDialogShow = false">取 消</el-button>
-        <el-button type="primary" @click="regDialogShow = false" @click="reg">确 定</el-button>
+        <el-button type="primary" @click="onSubmit">确 定</el-button>
     </div>
 </el-dialog>
 </template>
 
 <script>
-import {smPinCode} from '@/api/reg'
+import {userReg,smPinCode,sendSms,upload} from '@/api/reg'
 
 export default {
     name:'register',
     data(){
         return {
-            uploadUrl:'',
-            labelWidth:'62px',
+            upload:upload,
+            labelWidth:'70px',
             width:'603px',
             regDialogShow:false,
-            codePic:'',//验证码
+            tempImgUrl:'',
+            codePic:smPinCode(),//验证码
             regForm:{
-                avatar:'',
+                avatar:'',//头像
                 username:'',
                 email:'',
-                phone:'',
+                phone:'18317958001',
                 password:'',
+                code:'',//图形验证码
                 rcode:'',//手机验证码
             },
+            codeText:'获取用户验证码',
+            delay:0,//倒计时时长
             rules:{
                 avatar:[
                     { required: true, message: '请上传头像',trigger: 'change'},
@@ -93,29 +98,84 @@ export default {
                     { required: true, message: '请上输入密码',trigger: 'blur'},
                     { min: 6, message: '长度至少6个字符',trigger: 'blur'},
                 ],
+                rcode:[
+                    { required: true, message: '请输入手机验证码',trigger: 'blur'},
+                ],
             }
         };
     },
     methods:{
      handleAvatarSuccess(res, file) {
-        this.regForm.avatar = URL.createObjectURL(file.raw);
+        this.tempImgUrl = URL.createObjectURL(file.raw);//生成预览地址
+        this.regForm.avatar = res.data.file_path;//路径
       },
       beforeAvatarUpload(file) {
-        const isJPG = file.type === 'image/jpeg' || 'image/png';
-        const isLt2M = file.size / 1024 / 1024 < 2;
-
-        if (!isJPG) {
-          this.$message.error('上传头像图片只能是 JPG 格式!');
-        }
-        if (!isLt2M) {
-          this.$message.error('上传头像图片大小不能超过 2MB!');
-        }
-        return isJPG && isLt2M;
+      if(!upload.acceptType.includes(file.type.substr(file.type.indexOf('/')+1))){
+            this.$message.error(`上传头像图片只能是${upload.acceptType.join('|')}格式!`);
+            return false;
+      }
+      if(file.size >upload.sizeLimit){
+          this.$message.error(`上传头像图片大小不能超过${upload.sizeLimit/1024/1024}MB!`);
+          return false;
+      }
+        return true;
       },
       //验证码刷新
       refleshCode(){
            this.codePic = smPinCode();
-      }
+      },
+      //注册
+      onSubmit(){
+          this.$refs.regForm.validate(valid=>{
+                if(valid){
+                   userReg(this.regForm,res=>{
+                       console.log(res)
+                       this.$message.success('注册成功');
+                       this.regDialogShow = false;
+                       this.$refs.regForm.resetField();//清空表单
+                   })
+                }else{
+                    this.$message.warning('请输入正确的信息！');
+                }
+                
+            });
+      },
+      //获取验证码
+      getPhoneCode(){
+          //部分字段验证
+          this.$refs.regForm.validateField(['phone'],error=>{
+              if(error){
+                   this.$message.warning(error);
+                   return;
+               }
+               if(!/^\d{4}/.test(this.regForm.code)){
+                    this.$message.warning('请输入正确的图形码!');
+                    return;
+               }
+               
+               sendSms({
+                   phone:this.regForm.phone,
+                   code:this.regForm.code
+               },res=>{
+                    this.$message.success('验证码已发送！');
+                    let tptxt = this.codeText;
+                    this.delay = 10;
+                    let t = setInterval(() => {
+                        this.delay--
+                        this.codeText = `${this.delay}秒后重新获取`;
+                        if(this.delay < 1){
+                            clearInterval(t);
+                            this.codeText = tptxt;
+                            this.delay = 0;
+                            return
+                        }
+                    },1000);
+                    //测试代码,实际不可操作
+                    this.regForm.rcode = res.captcha;
+
+               });
+          });
+        }
     }
 }
 </script>
@@ -163,7 +223,7 @@ export default {
     line-height: 178px;
     text-align: center;
   }
-  .avatar {
+  .head-avatar {
     width: 178px;
     height: 178px;
     display: block;
